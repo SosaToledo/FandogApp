56eec8228ce968a40b104115eb6db7357cfe26fd
frank <frank_toledo_009@hotmail.com>
2018-01-07 20:52:45 -0300

    ui

--- app/src/main/java/ar/com/thinco/fandog/miPagerAdapter.java
+++ app/src/main/java/ar/com/thinco/fandog/miPagerAdapter.java
@@ -1,8 +1,42 @@
 package ar.com.thinco.fandog;
 
-/**
- * Created by Frank on 6/1/2018.
- */
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
 
-public class miPagerAdapter {
+public class miPagerAdapter extends FragmentPagerAdapter {
+    private static final CharSequence FRAG0 = "Salchichas";
+    private static final CharSequence FRAG1 = "Panes";
+    private static final CharSequence FRAG2 = "Gaseosa de Vidrio";
+
+
+    private static final int COUNT = 3;
+
+    public miPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return new ArticuloFragment();
+    }
+
+    @Override
+    public int getCount() {
+        return COUNT;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position){
+            case 0:
+                return FRAG0;
+            case 1:
+                return FRAG1;
+            case 2:
+                return FRAG2;
+            default:
+                return null;
+        }
+    }
 }
